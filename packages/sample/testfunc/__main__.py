eb875bccdf3c0a294ded29dc401a2a67fc5db8ed
techswarn <tech.swarns@gmail.com>
2024-06-19 10:22:00 +0530

    testing

--- packages/sample/testfunc/__main__.py
+++ packages/sample/testfunc/__main__.py
@@ -1,4 +1,4 @@
-import psycopg2
+import psycopg
 from dotenv import load_dotenv
 import os
 
@@ -9,6 +9,31 @@ import os
 load_dotenv()
 
 def main(event, context):
-    connection = psycopg2.connect(database=os.environ['DATABASE'], user=os.environ['USERNAME'], password=os.environ['PASSWORD'], host=os.environ['HOST'], port=os.environ['PORT'])
-    cursor = connection.cursor()
+    with psycopg.connect(dbname=os.environ['DATABASE'], user=os.environ['USERNAME'], password=os.environ['PASSWORD'], host=os.environ['HOST'], port=os.environ['PORT']) as conn:
+
+    # Open a cursor to perform database operations
+        with conn.cursor() as cur:
+            # Execute a command: this creates a new table
+        # Execute a command: this creates a new table
+            cur.execute("""
+                CREATE TABLE test (
+                    id serial PRIMARY KEY,
+                    num integer,
+                    data text)
+                """)
+        # Pass data to fill a query placeholders and let Psycopg perform
+        # the correct conversion (no SQL injections!)
+            cur.execute(
+                "INSERT INTO test (num, data) VALUES (%s, %s)",
+                (100, "abc'def"))
+            
+            cur.execute("SELECT * FROM test")
+            cur.fetchone()
+            # will return (1, 100, "abc'def")
+
+            # You can use `cur.fetchmany()`, `cur.fetchall()` to return a list
+            # of several records, or even iterate on the cursor
+            for record in cur:
+                print(record)
+            conn.commit()
     return {"body": 'Hello there'}
